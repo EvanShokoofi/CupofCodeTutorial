8ca498af6538699a5d5df38aa8b3f40e7ecb86d6
Evan Shokoofi <24443652+EvanShokoofi@users.noreply.github.com>
2018-07-05 10:19:42 -0700

    asdf

--- Day_11.py
+++ Day_11.py
@@ -14,3 +14,28 @@ for row in range(row_num):
 print(list, '\n')
 
 
+#write a Python program that accepts a sequence of lower case lines (blank line to terminate) as input
+# and prints the lines as output (all characters in upper case)
+
+lines = []
+while True:
+	l = input()
+	if l:
+		lines.append(l.lower())
+	else:
+		break;
+for l in lines:
+	print(l)
+
+
+#write a program which accepts a sequence of comma separated 4 digit binary numbers as its input
+# and print the numbers that are divisible by 5 in a cs sequence.
+
+def divividable_binary():
+	s = input('Enter the binaries: ')
+	l = s.split(',')
+	for i in l:
+		if int(str(i),2) % 5 == 0:
+			print('Output: ',i)
+divividable_binary()
+
